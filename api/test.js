eb7ad54ee52a7542e8950ad5c4e715d528ccd4ef
casval0078 <167587339+casval0078@users.noreply.github.com>
2026-06-02 16:31:56 +0900

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,7 +1,52 @@
 export default async function handler(req, res) {
 
-  res.status(200).json({
-    reply: "Vercel Function成功"
-  });
+  try {
+
+    const { message } = req.body;
+
+    const response =
+    await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                {
+                  text: message
+                }
+              ]
+            }
+          ]
+        })
+      }
+    );
+
+    const data =
+    await response.json();
+
+    const reply =
+      data?.candidates?.[0]
+      ?.content?.parts?.[0]
+      ?.text
+      || "返答なし";
+
+    res.status(200).json({
+      reply
+    });
+
+  } catch(err){
+
+    console.error(err);
+
+    res.status(500).json({
+      reply:"エラー"
+    });
+
+  }
 
 }
